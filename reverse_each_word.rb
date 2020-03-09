89f995305ddbd9021b0a881a40d2b89947f3aa38
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-09 00:20:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,13 @@ new_array = string.split(" ")
   end
   return_array.join(" ")
 end
+
+
+def reverse_each_word_collect(string)
+  new_array = string.split(" ")
+  return_array = []
+  new_array.collect do |string|
+    return_array << string.reverse
+  end 
+  return_array.join(" ")
+end 
